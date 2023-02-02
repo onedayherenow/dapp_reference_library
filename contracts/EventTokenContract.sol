e824e20130f1736c16e3b1cffa10d369d6f2715f
onedayherenow <onedayherenow@gmail.com>
2023-02-02 15:46:55 -0500

    thank you

--- contracts/EventTokenContract.sol
+++ contracts/EventTokenContract.sol
@@ -12,7 +12,7 @@ pragma solidity ^0.8.13;
 /// @notice Explain to an end user what this does
 /// @dev Explain to a developer any extra details
 
-contract Events2 {
+contract EventTokenContract {
 
     //constructor
     constructor(address payable _wallet, ERC20Token _token) public {
@@ -30,7 +30,7 @@ contract Events2 {
     //structs
 
     //mapping
-    mapping(address => uint256) public balances;  // this mapping here gives us a balance value for each address/mapping
+
 
 
     /////////////
@@ -39,26 +39,26 @@ contract Events2 {
     //crud
 
     //functions
-    function buyToken() public payable {
+    // REFACTORED CODE (LOOK AT EVENT2.SOL FOR ORIGINAL)
+    // ! ! ! ! ! ! ! ! ! !   
+    function buyTokenOriginal() public payable {
         ERC20Token _token = ERC20Token(address(token));
         _token.mint();  // minting 'instantiates' data into working memory, into the blockchain - written to blockchain
         wallet.transfer(msg.value);
     }
 
 
+    function buyToken() public payable {
+        ERC20Token(address(token)).mint();
+        wallet.transfer(msg.value);
+    }
+
+
+
     // function mint() public {              
     //    balances[msg.sender] += 1;     <----------- We do not use msg.sender 
     //                                   <----------- because it always returns the address that started the contract
     // }                                 <----------- so we use tx.origin instead to reference the address that invoked the transaction
-    
-
-    // msg.sender can be a contract
-    // tx.origin can never be a contract
-
-    function mint() public {
-        balances[tx.origin] += 1;      
-    
-    }
 
 
     //event    ---->  listen to them directly,, it's nice to call a function and see an event triggered as a confirmation test
